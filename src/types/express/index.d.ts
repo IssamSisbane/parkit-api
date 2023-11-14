e9049c9467e04a39103eeba4b9f0d51cf24c5196
Issam SISBANE <issam.sisbane@etu.univ-lyon1.fr>
2023-11-14 09:19:00 +0100

    fix dev server issues

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,12 +1,9 @@
 import { IUser } from "~/interfaces/user.interface";
-// import { Request } from "express"
-
-export { };
 
 declare global {
     namespace Express {
         interface Request {
-            user: IUser;
+            user?: IUser
         }
     }
 }
