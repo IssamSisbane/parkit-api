5d76207cf6066df5b9de76089909d328393d09d0
Issam SISBANE <issam.sisbane@etu.univ-lyon1.fr>
2023-11-14 11:04:18 +0100

    add dev config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,8 +1,10 @@
-import { DB_HOST, DB_PORT, DB_DATABASE, DB_ADMIN_USERNAME, DB_ADMIN_PASSWORD } from '~/config/env.config';
+import { NODE_ENV, DB_HOST, DB_PORT, DB_DATABASE, DB_ADMIN_USERNAME, DB_ADMIN_PASSWORD } from '~/config/env.config';
 
 export const dbConnection = {
-    url: `mongodb+srv://${DB_ADMIN_USERNAME}:${DB_ADMIN_PASSWORD}@${DB_HOST}/${DB_DATABASE}?retryWrites=true&w=majority`,
-    //url: `mongodb://${DB_ADMIN_USERNAME}:${DB_ADMIN_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`,
+    url:
+        NODE_ENV == 'production'
+            ? `mongodb+srv://${DB_ADMIN_USERNAME}:${DB_ADMIN_PASSWORD}@${DB_HOST}/${DB_DATABASE}?retryWrites=true&w=majority`
+            : `mongodb://${DB_ADMIN_USERNAME}:${DB_ADMIN_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`,
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
